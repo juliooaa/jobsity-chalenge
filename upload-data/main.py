503e6ec5594a55176969aac61fda74562efa06a3
Julio Oliveira <julimaraujo93@gmail.com>
2022-04-24 20:21:53 -0300

    Update main

--- upload-data/main.py
+++ upload-data/main.py
@@ -1,11 +1,13 @@
 import boto3
 import os
 
-path = '/home/juliooliveira/Área de Trabalho/julio/chalenge/upload-data/data'
+## Change ${Path} according to your project path
+path = '${Path}/data'
 file = 'trips.csv'
 
 print(os.path.join(path, file))   
 
+## Upload data for s3 bucket
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('chalenge-trips-jobsity')
 s3.Object('chalenge-trips-jobsity', 'trips.csv').put(Body=open(os.path.join(path, file), 'rb'))
