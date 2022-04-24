abffdb199cfbd27336d3eb0fc3974ac26ba37070
Julio Oliveira <julimaraujo93@gmail.com>
2022-04-24 20:19:46 -0300

    Fixing Path

--- upload-data/ingest-job-spark.py
+++ upload-data/ingest-job-spark.py
@@ -1,11 +1,13 @@
 import boto3
 import os
 
-path = '/home/juliooliveira/Área de Trabalho/julio/chalenge'
+## Change ${Path} according to your project path
+path = '${Path}'
 file = 'spark.py'
 
 print(os.path.join(path, file))   
 
+## Upload the files to s3 bucket
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('spark-glue-job')
 s3.Object('spark-glue-job', 'spark.py').put(Body=open(os.path.join(path, file), 'rb'))
