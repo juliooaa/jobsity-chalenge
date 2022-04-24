7c79e5dd615b393521122982e6dd5358e781d7ed
Julio Oliveira <julimaraujo93@gmail.com>
2022-04-24 20:22:33 -0300

    fixing commit

--- upload-data/ingest-job-spark.py
+++ upload-data/ingest-job-spark.py
@@ -7,7 +7,7 @@ file = 'spark.py'
 
 print(os.path.join(path, file))   
 
-## Upload the files to s3 bucket
+## Upload file to s3 bucket
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('spark-glue-job')
 s3.Object('spark-glue-job', 'spark.py').put(Body=open(os.path.join(path, file), 'rb'))
